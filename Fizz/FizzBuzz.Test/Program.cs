5f2d7bae69fc567029fd09c56114a86d308fbcf9
Nairodie <42880967+Nairodie@users.noreply.github.com>
2018-10-15 17:23:24 -0500

    Add files via upload

---
+++ Fizz/FizzBuzz.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ Fizz/FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 1; i <= 100; i++)
+           {
+               Console.WriteLine(FizzBuzz.Output(i));
+           }
+
+           Console.WriteLine("Press enter to exit");
+           Console.ReadLine();
+        }
+    }
+}
