c826be0d1638d9bf3a5e4cc8f83bff19681f2311
Nairodie <42880967+Nairodie@users.noreply.github.com>
2018-12-13 17:42:22 +0000

    Update FizzBuzzTests.cs

--- Fizz/FizzBuzz.Test/FizzBuzzTests.cs
+++ Fizz/FizzBuzz.Test/FizzBuzzTests.cs
@@ -6,7 +6,7 @@ namespace FizzBuzz.Tests
     public class FizzBuzzTests
     {
         [Test]
-        public void FizzBuzz_GetOutput_Method_Returns_Fizz_When_Number_Is_Divisible_By_Only_3()
+        public void FizzBuzz_Returns_Fizz_When_Number_Is_Divisible_By_3()
         {
             Assert.That(FizzBuzz.GetOutput(3), Is.StringMatching("Fizz"));
             Assert.That(FizzBuzz.GetOutput(6), Is.StringMatching("Fizz"));
@@ -14,7 +14,7 @@ namespace FizzBuzz.Tests
         }
 
         [Test]
-        public void FizzBuzz_GetOutput_Method_Returns_Buzz_When_Number_Is_Divisible_By_Only_5()
+        public void FizzBuzz_Returns_Buzz_When_Number_Is_Divisible_5()
         {
             Assert.That(FizzBuzz.GetOutput(5), Is.StringMatching("Buzz"));
             Assert.That(FizzBuzz.GetOutput(10), Is.StringMatching("Buzz"));
@@ -22,7 +22,7 @@ namespace FizzBuzz.Tests
         }
 
         [Test]
-        public void FizzBuzz_GetOutput_Method_Returns_FizzBuzz_When_Number_Is_Divisible_By_3_And_5()
+        public void FizzBuzz_Returns_FizzBuzz_When_Number_Is_Divisible_By_3_And_5()
         {
             Assert.That(FizzBuzz.GetOutput(15), Is.StringMatching("FizzBuzz"));
             Assert.That(FizzBuzz.GetOutput(30), Is.StringMatching("FizzBuzz"));
